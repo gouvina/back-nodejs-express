7a3bd8371bb59e79ef74d0b0f87715a1289f1555
gouvina <germanouvina@gmail.com>
2025-06-03 17:13:41 -0300

    chore: updated user.router comments

--- src/user/user.router.js
+++ src/user/user.router.js
@@ -15,19 +15,19 @@ const router = express.Router();
 
 // ENDPOINTS
 
-// GET /users - Gets list of users
+// GET /users - Gets list of all users in the DB
 router.get('/', getUsers);
 
-// POST /users - Creates a new user
+// POST /users - Creates a new user in the DB
 router.post('/', createUser);
 
-// GET /users/:id - Gets list of users
+// GET /users/:id - Gets specific user from the DB by id
 router.get('/:id', getUser);
 
-// PUT /users/id - Gets list of users
+// PUT /users/id - Updates specific user in the DB by id
 router.put('/:id', updateUser);
 
-// DELETE /users/id - Gets list of users
+// DELETE /users/id - Deletes specific user from the DB by id
 router.delete('/:id', deleteUser);
 
 module.exports = router;
